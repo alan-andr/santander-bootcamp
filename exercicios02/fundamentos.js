6474a3545c78ee664dacee99649410a4700e86ae
alan-andr <alanandrade4985@gmail.com>
2025-07-26 09:47:23 -0300

    .

---
+++ exercicios02/fundamentos.js
@@ -0,0 +1,4 @@
+let nome = "João";
+let idade = 25;
+
+console.log(`Olá, meu nome é ${nome} e tenho ${idade} anos.`);
---
+++ exercicios02/operadoresAritmeticos.js
@@ -0,0 +1,23 @@
+
+function soma(a, b) {
+    return a + b;
+}
+
+function subtrai(a, b) {
+    return a - b;
+}
+
+function multiplica(a, b) {
+    return a * b;
+}
+
+function divisao(a, b) {
+    return a / b;
+}
+
+console.log(soma(10, 5))
+console.log(subtrai(10, 5));
+console.log(multiplica(10, 5));
+console.log(divisao(10, 5));
+
+
