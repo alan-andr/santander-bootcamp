ba28a7f6e7fc933d2b220a86fe2906f5c9dd33d0
Alan Andrade <alanandrade4985@gmail.com>
2025-08-08 11:23:46 -0300

    consumindo a pokeApi

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -2,5 +2,30 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url);
+function convertPokemonToLi(pokemon) {
+    return `<li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
 
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">Grass</li>
+                        <li class="type">Poison</li>
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="${pokemon.name}">
+                </div>
+            </li>`
+}
+
+const pokemonList = document.getElementById('pokemonList');
+
+PokeApi.getPokemons().then((pokemons) => {
+    const listItens = [];
+
+    for(let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        listItens.push(convertPokemonToLi(pokemon));
+    }
+
+    console.log(listItens)
+})
